947a8eacc345536e5713d12ce8cd4e19f4ac22ba
Levi Lesches <levilesches@gmail.com>
2025-02-16 17:15:34 -0500

    Fill all pumps individually

--- Science/src/pumps/pumps.cpp
+++ Science/src/pumps/pumps.cpp
@@ -1,30 +1,41 @@
 #include "pumps.h"
 
-const int pumpDelay = 2000;  // milliseconds
+const int pump1_pin = 21;  // H2O2
+const int pump2_pin = 22;  // Acetone
+const int pump3_pin = 23;  // BCA
+const int pump4_pin = 39;  // Buffer
+const int pump5_pin = 38;  // Distilled water
+
+// Total delay: 33 seconds
+const int pump1_delay = 6600;
+const int pump2_delay = 3300;
+const int pump3_delay = 9800;
+const int pump4_delay = 10'000;
+const int pump5_delay = 3300;
 
 void Pumps::setup() {
-  pinMode(PUMP1, OUTPUT);
-  pinMode(PUMP2, OUTPUT);
-  pinMode(PUMP3, OUTPUT);
-  pinMode(PUMP4, OUTPUT);
-  pinMode(PUMP5, OUTPUT);
+  pinMode(pump1_pin, OUTPUT);
+  pinMode(pump2_pin, OUTPUT);
+  pinMode(pump3_pin, OUTPUT);
+  pinMode(pump4_pin, OUTPUT);
+  pinMode(pump5_pin, OUTPUT);
   turnOff();
 }
 
 void Pumps::turnOn() {
-  digitalWrite(PUMP1, HIGH);
-  digitalWrite(PUMP2, HIGH);
-  digitalWrite(PUMP3, HIGH);
-  digitalWrite(PUMP4, HIGH);
-  digitalWrite(PUMP5, HIGH);
+  digitalWrite(pump1_pin, HIGH);
+  digitalWrite(pump2_pin, HIGH);
+  digitalWrite(pump3_pin, HIGH);
+  digitalWrite(pump4_pin, HIGH);
+  digitalWrite(pump5_pin, HIGH);
 }
 
 void Pumps::turnOff() {
-  digitalWrite(PUMP1, LOW);
-  digitalWrite(PUMP2, LOW);
-  digitalWrite(PUMP3, LOW);
-  digitalWrite(PUMP4, LOW);
-  digitalWrite(PUMP5, LOW);
+  digitalWrite(pump1_pin, LOW);
+  digitalWrite(pump2_pin, LOW);
+  digitalWrite(pump3_pin, LOW);
+  digitalWrite(pump4_pin, LOW);
+  digitalWrite(pump5_pin, LOW);
 }
 
 void Pumps::handleCommand(ScienceCommand command) { 
@@ -38,7 +49,30 @@ void Pumps::handleCommand(ScienceCommand command) {
 }
 
 void Pumps::fillTubes() {
-  turnOn();
-  delay(pumpDelay);
+  Serial.println("Pouring pump 1");
+  digitalWrite(pump1_pin, HIGH);
+  delay(pump1_delay);
+  digitalWrite(pump1_pin, LOW);
+
+  Serial.println("Pouring pump 2");
+  digitalWrite(pump2_pin, HIGH);
+  delay(pump2_delay);
+  digitalWrite(pump2_pin, LOW);
+
+  Serial.println("Pouring pump 3");
+  digitalWrite(pump3_pin, HIGH);
+  delay(pump3_delay);
+  digitalWrite(pump3_pin, LOW);
+
+  Serial.println("Pouring pump 4");
+  digitalWrite(pump4_pin, HIGH);
+  delay(pump4_delay);
+  digitalWrite(pump4_pin, LOW);
+
+  Serial.println("Pouring pump 5");
+  digitalWrite(pump5_pin, HIGH);
+  delay(pump5_delay);
+  digitalWrite(pump5_pin, LOW);
+
   turnOff();
 }
--- Science/src/pumps/pumps.h
+++ Science/src/pumps/pumps.h
@@ -2,12 +2,6 @@
 
 #include "../science.pb.h"
 
-#define PUMP1 21
-#define PUMP2 22
-#define PUMP3 23
-#define PUMP4 38
-#define PUMP5 39
-
 class Pumps {
   public:
     void setup();
